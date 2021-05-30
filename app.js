7f2962b98f85154b9084a856f6d0e1cf6e16cab6
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-30 21:11:09 +0000

    Serve static files

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello world!')
+// })
+
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`App listening at http:localhost:${port}`)
 })
+
