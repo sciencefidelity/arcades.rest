6d732c0ca4fc8af7d577ed85da67018d17874e8a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-30 22:45:37 +0000

    Set up sample data

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config()
 
-// app.get('/', (req, res) => {
-//   res.send('Hello world!')
-// })
+const connectionString = process.env.MONGO_ATLAS_PASSWORD
 
-app.use(express.static('public'))
+const mongoose = require('mongoose')
+const userSchema = require('./userSchema.js')
+const User = mongoose.model('user'. userSchema, 'user')
 
-app.listen(port, () => {
-  console.log(`App listening at http:localhost:${port}`)
-})
+async function createUser(username) {
+  return new User({
+    username,
+    created: Date.now()
+  }).save()
+}
+
+async function findUser(username) {
+  return await User.findOne({ username })
+}
+
+;(async () => {
+  const connector = mongoose.connect(connectionString)
+  const username = process.argv[2].split('=')[1]
+
+  let user = await connector.then(async () => {
+    return findUser(username)
+  })
+
+  if (!user) {}
+  user = await createUser(username)
+
+  console.log(user)
+  process.exit(0)
+})()
 
