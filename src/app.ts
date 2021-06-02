d8e7753c4b7efa550f273cbf3789ca436c0bc12a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-02 14:00:13 +0000

    Create test app for koa

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,8 @@
-import * as dotenv from 'dotenv'
-import { model, connect } from 'mongoose'
-import { User, userSchema } from './userSchema'
+import Koa from 'koa'
+const app = new Koa()
 
-dotenv.config({ path: __dirname+'/.env' })
+app.use(async ctx => {
+  ctx.body = 'Hello world!'
+})
 
-const connectionString = process.env.MONGO_ATLAS_STRING
-
-const UserModel = model<User>('User', userSchema)
-
-async function createUser(username:string) {
-  return new UserModel({
-    username,
-    created: Date.now()
-  }).save()
-}
-
-async function findUser(username:string) {
-  return await UserModel.findOne({ username })
-}
-
-run().catch(err => console.log(err))
-
-async function run(): Promise<void> {
-  const connector = connect(connectionString!)
-  const username = process.argv[2].split('=')[1]
-
-  let user = await connector.then(async () => {
-    return findUser(username)
-  })
-
-  if (!user) {
-    user = await createUser(username)
-  }
-
-  console.log(user)
-  process.exit(0)
-}
+app.listen(3000)
---
+++ src/createUser.ts
@@ -0,0 +1,38 @@
+import * as dotenv from 'dotenv'
+import { model, connect } from 'mongoose'
+import { User, userSchema } from './userSchema'
+
+dotenv.config({ path: __dirname+'/.env' })
+
+const connectionString = process.env.MONGO_ATLAS_STRING
+
+const UserModel = model<User>('User', userSchema)
+
+async function createUser(username:string) {
+  return new UserModel({
+    username,
+    created: Date.now()
+  }).save()
+}
+
+async function findUser(username:string) {
+  return await UserModel.findOne({ username })
+}
+
+run().catch(err => console.log(err))
+
+async function run(): Promise<void> {
+  const connector = connect(connectionString!)
+  const username = process.argv[2].split('=')[1]
+
+  let user = await connector.then(async () => {
+    return findUser(username)
+  })
+
+  if (!user) {
+    user = await createUser(username)
+  }
+
+  console.log(user)
+  process.exit(0)
+}
