b424c1ecb8f09aa1397fabc9b6659f63da5ed1c3
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-03 10:49:03 +0000

    Experiment with routes

---
+++ src/routeId.ts
@@ -0,0 +1,18 @@
+import koa = require('koa')
+import Router = require('koa-router')
+
+const app = new koa()
+const port = 3000
+const router = new Router()
+
+router.get('/:name', (ctx, next) => {
+  ctx.body = 'The coolest person is ' + ctx.params.name + '!'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(port, () => {
+console.log(`Listening at http://localhost:${port}`)
+})
