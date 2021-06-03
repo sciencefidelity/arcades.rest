1d0afbf928ac2d5dc020e9c3cf30bc2212e6f219
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-03 14:10:50 +0000

    Create head template, link to stylesheet

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
+import * as dotenv from 'dotenv'
 import Koa from 'koa'
 import Path from 'path'
-import Router from 'koa-router'
 import Pug from 'koa-pug'
+import Router from 'koa-router'
+import Serve from 'koa-static'
+
+dotenv.config({ path: '.env' })
 
 const app = new Koa()
-const port = 3000
+const port = process.env.PORT || 3000
 const router = new Router()
 
 const pug = new Pug({
@@ -13,9 +17,12 @@ const pug = new Pug({
   app: app
 })
 
+const styles = Path.resolve(__dirname, './views/css/style.css')
+
 router.get('/:name', async (ctx) => {
   await ctx.render('index', {
-    name: ctx.params.name.charAt(0).toUpperCase() + ctx.params.name.slice(1)
+    name: ctx.params.name.charAt(0).toUpperCase() + ctx.params.name.slice(1),
+    styles: styles
   })
 })
 
@@ -28,6 +35,8 @@ const handle404Errors = (ctx: any) => {
    if (404 != ctx.status) return;
    ctx.redirect('/not_found');
 }
+
+app.use(Serve(Path.join(__dirname, 'public')))
 
 app
   .use(router.routes())
