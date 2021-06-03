896fd61d70f61482cbd56c354faeaf67727feb2e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-03 12:50:41 +0000

    Set up routes with pug templates

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,36 @@
-import Koa = require('koa')
-import path = require('path')
+import Koa from 'koa'
+import Path from 'path'
+import Router from 'koa-router'
 import Pug from 'koa-pug'
 
 const app = new Koa()
 const port = 3000
+const router = new Router()
 
 const pug = new Pug({
-  viewPath: path.resolve(__dirname, './views'),
-  basedir: path.resolve(__dirname, './views'),
-  app
+  viewPath: Path.resolve(__dirname, './views'),
+  basedir: Path.resolve(__dirname, './views'),
+  app: app
 })
 
-app.use(async ctx => {
-  await ctx.render('index', true)
+router.get('/hello', async (ctx) => {
+  await ctx.render('index')
 })
+
+router.get('/not_found', async (ctx) => {
+  ctx.status = 404
+  await ctx.render('404')
+})
+
+const handle404Errors = (ctx: any) => {
+   if (404 != ctx.status) return;
+   ctx.redirect('/not_found');
+}
+
+app
+  .use(router.routes())
+  .use(handle404Errors)
+  .use(router.allowedMethods())
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
