74b3ab0bbccd270b48479370461834071b6385eb
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-03 01:39:01 +0000

    Display local ip in console

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
+import * as dotenv from 'dotenv'
 import Koa from 'koa'
+import serve from 'koa-static'
+import { networkInterfaces } from 'os'
 
-const app = new Koa()
-const port = 3000
+dotenv.config({ path: '.env' })
 
-app.use(async ctx => {
-  ctx.body = 'Hello matt!'
+// init koa server
+const server = new Koa()
+const port = process.env.PORT || 3000
+
+// get the local network address
+let address:string
+networkInterfaces().en0?.filter(details => {
+  if (details.family === 'IPv4') {
+    address = details.address
+  }
 })
 
-app.listen(port, () => {
+// serve static pages
+server.use(serve('src/public'))
+
+// start server
+server.listen(port, () => {
   console.log(`listening on http://localhost:${port}`)
+  console.log(`local network http://${address}:${port}`)
 })
