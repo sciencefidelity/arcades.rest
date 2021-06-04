d14435e19b19113be7f1fbd25b92e6409ad34a35
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-04 22:28:43 +0000

    Send a cookie

---
+++ src/upload.ts
@@ -0,0 +1,46 @@
+import * as dotenv from 'dotenv'
+import Koa from 'koa'
+import Router from 'koa-router'
+import koaBody from 'koa-body'
+import Path from 'path'
+import Pug from 'koa-pug'
+
+// load environment variables
+dotenv.config({ path: '.env' })
+
+// set up middleware
+const app = new Koa()
+const port = process.env.PORT || 3000
+const router = new Router()
+
+const pug = new Pug({
+  viewPath: Path.resolve(__dirname, './views'),
+  basedir: Path.resolve(__dirname, './views'),
+  app: app
+})
+
+app.use(koaBody({
+  formidable:{uploadDir: Path.resolve(__dirname, './uploads')},
+  multipart: true,
+  urlencoded: true
+}))
+
+async function renderForm(ctx: any) {
+  await ctx.render('file_upload')
+}
+
+const handleForm = (ctx: any) => {
+  console.log('Files: ', ctx.request.files)
+  console.log('Fields: ', ctx.request.body.fields)
+  ctx.body = 'Recieved your data!'
+}
+
+router.get('/files', renderForm)
+router.post('/upload', koaBody(), handleForm)
+
+// start server
+app.use(router.routes())
+   .use(router.allowedMethods())
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
