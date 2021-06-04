d98d6285cc607e5bf951700ace132e92c9a7c4ad
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-04 20:16:41 +0000

    Set up body parser, build form tempate

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import * as dotenv from 'dotenv'
 import Koa from 'koa'
+import Router from 'koa-router'
+import Body from 'koa-body'
 import Path from 'path'
 import Pug from 'koa-pug'
-import Router from 'koa-router'
-import Serve from 'koa-static'
 
+// load environment variables
 dotenv.config({ path: '.env' })
 
+// set up middleware
 const app = new Koa()
 const port = process.env.PORT || 3000
 const router = new Router()
@@ -17,32 +19,29 @@ const pug = new Pug({
   app: app
 })
 
-const styles = Path.resolve(__dirname, './views/css/style.css')
+// set up body parsing middleware
+app.use(Body({
+  formidable:{uploadDir: './uploads'},
+  multipart: true,
+  urlencoded: true
+}))
 
-router.get('/:name', async (ctx) => {
-  await ctx.render('index', {
-    name: ctx.params.name.charAt(0).toUpperCase() + ctx.params.name.slice(1),
-    styles: styles
-  })
-})
-
-router.get('/not_found', async (ctx) => {
-  ctx.status = 404
-  await ctx.render('404')
-})
-
-const handle404Errors = (ctx: any) => {
-   if (404 != ctx.status) return;
-   ctx.redirect('/not_found');
+async function renderForm(ctx: any) {
+  await ctx.render('form')
 }
 
-app.use(Serve(Path.join(__dirname, 'public')))
+const handleForm = (ctx: any) => {
+  console.log(ctx.request.body)
+  console.log(ctx.req.body)
+  ctx.body = ctx.request.body
+}
 
-app
-  .use(router.routes())
-  .use(handle404Errors)
-  .use(router.allowedMethods())
+router.get('/', renderForm)
+router.post('/', handleForm)
 
+// start server
+app.use(router.routes())
+   .use(router.allowedMethods())
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
---
+++ src/templates.ts
@@ -0,0 +1,51 @@
+import * as dotenv from 'dotenv'
+import Koa from 'koa'
+import Path from 'path'
+import Pug from 'koa-pug'
+import Router from 'koa-router'
+import Serve from 'koa-static'
+
+// load environment variables
+dotenv.config({ path: '.env' })
+
+// set up middleware
+const app = new Koa()
+const port = process.env.PORT || 3000
+const router = new Router()
+
+// load html templates
+const pug = new Pug({
+  viewPath: Path.resolve(__dirname, './views'),
+  basedir: Path.resolve(__dirname, './views'),
+  app: app
+})
+
+// load stylesheet
+const styles = Path.resolve(__dirname, './views/css/style.css')
+
+// set up routes
+router.get('/:name', async (ctx) => {
+  await ctx.render('index', {
+    name: ctx.params.name.charAt(0).toUpperCase() + ctx.params.name.slice(1),
+    styles: styles
+  })
+})
+
+// handle 404 errors
+router.get('/not_found', async (ctx) => {
+  ctx.status = 404
+  await ctx.render('404')
+})
+const handle404Errors = (ctx: any) => {
+   if (404 != ctx.status) return;
+   ctx.redirect('/not_found');
+}
+
+// start server
+app.use(Serve(Path.join(__dirname, 'public')))
+   .use(router.routes())
+   .use(handle404Errors)
+   .use(router.allowedMethods())
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
