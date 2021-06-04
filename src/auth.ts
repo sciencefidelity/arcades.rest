f2c5c886461c931313c946f01f73c8c2dbd89252
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-04 23:16:35 +0000

    Try some basic auth

---
+++ src/auth.ts
@@ -0,0 +1,39 @@
+import Koa from 'koa'
+import Auth from 'koa-basic-auth'
+import Router from 'koa-router'
+
+const credentials = { name: 'matt', pass: 'isnotcool' }
+const app = new Koa()
+const port = process.env.PORT || 3000
+const router = new Router()
+
+// custom 401 handling
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    if (401 == err.status) {
+      ctx.status = 401
+      ctx.set('WWW-Authenticate', 'Basic')
+      ctx.body = 'Not today thank you.'
+    } else {
+      throw err
+    }
+  }
+})
+
+// unprotected route
+router.get('/protected', Auth(credentials), ctx => {
+  ctx.body = 'You\'re on the guest list.'
+})
+
+// protected route
+router.get('/unprotected', ctx => {
+  ctx.body = 'Free for all!'
+})
+
+app.use(router.routes())
+   .use(router.allowedMethods())
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
---
+++ src/session.ts
@@ -0,0 +1,28 @@
+import Koa from 'koa'
+import Session from 'koa-session'
+
+const app = new Koa()
+const port = process.env.PORT || 3000
+
+const CONFIG = {
+  maxAge: 86400000
+}
+
+app.keys = [ 'A super secret key.' ]
+app.use(Session(CONFIG, app))
+
+app.use(ctx => {
+  if (ctx.path === '/favicon.ico') return
+
+  let n = ctx.session!.views || 0
+  ctx.session!.views = ++n
+
+  if(n === 1)
+    ctx.body = 'It\'s my first time!'
+  else
+    ctx.body = 'You\'ve done this ' + n + ' times!'
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
