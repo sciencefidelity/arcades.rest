3d9296e72a9705780126efc9d3e7de7868de068c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-12 12:09:30 +0000

    Creat initial routes for arcades

--- src/routes/arcades.ts
+++ src/routes/arcades.ts
@@ -1,10 +1,38 @@
 import Router from 'koa-router'
-import * as arcades from '../data/example.json'
+import { arcadesModel } from '../models/arcadesSchema'
 
 const router = new Router({ prefix: '/arcades' })
 
+// get all arcades
 router.get('/', async (ctx, next) => {
-  ctx.body = arcades
+  try {
+    ctx.body = await arcadesModel.find({})
+    if (!ctx.body[0]) ctx.throw(404)
+  } catch (err) {
+    ctx.status = err.status || 500
+  }
+})
+
+// add a user
+router.post('/', async (ctx, next) => {
+  let { index } = ctx.request.body
+  // check if data is application/json
+  if(!ctx.is('application/json')) {
+    ctx.throw(412, 'content-Type must be application/json')
+  }
+  try {
+    // check if the arcade exists before creating
+    let arcade = await arcadesModel.findOne({ index })
+    // if user doesn't exist - create arcade
+    if(!arcade) {
+      arcade = await new arcadesModel(ctx.request.body).save()
+      ctx.status = 201
+    }
+    ctx.body = arcade
+  // error handling
+  } catch (err) {
+    ctx.throw(422, 'missing content')
+  }
 })
 
 export default router
