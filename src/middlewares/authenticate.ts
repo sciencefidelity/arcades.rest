c5a62b050d119098c3b3a1bfcb6ae67878aac02a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-07-27 12:28:21 +0000

    Move async/await out of promise constructor

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,28 +1,32 @@
 import { compare } from "bcryptjs"
 import { UserModel } from "../models/userSchema"
 
-export const Authenticate = (username:string, password:string) => {
-  // eslint-disable-next-line space-before-function-paren
-  return new Promise(async (resolve, reject) => {
-    try {
-      // get user by email
-      const user = await UserModel.findOne({ username })
-      if (!user) {
-        throw(422)
-      }
-      // match password
-      compare(password, user.password, (err, isMatch) => {
-        if (err) throw err
-        if (isMatch) {
-          resolve(user)
-        } else {
-          // password didn't match
-          reject("authentication failed")
+export const Authenticate = (username:string, password:string): Promise<unknown> => {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line space-before-function-paren
+    (async () => {
+      try {
+        // get user by email
+        const user = await UserModel.findOne({ username })
+        if (!user) {
+          throw new Error("authentication failed")
         }
-      })
-    } catch (err) {
-      // email not found
-      reject("authentication failed")
-    }
+        if (user) {
+          // match password
+          compare(password, user.password, (err, isMatch) => {
+            if (err) throw err
+            if (isMatch) {
+              resolve(user)
+            } else {
+              // password didn't match
+              reject(new Error("authentication failed"))
+            }
+          })
+        }
+      } catch (err) {
+        // email not found
+        reject(new Error("authentication failed"))
+      }
+    })()
   })
 }
