224b2048157fdf7f136e918b90d37b06aa4df3ab
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-10 17:07:51 +0000

    build: add eslint rules, set prettier to use all defaults, quick fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,42 @@
 module.exports = {
-  plugins: ["@typescript-eslint"],
+  plugins: [
+    "@typescript-eslint",
+    "eslint-comments",
+    "jest",
+    "promise",
+    "unicorn",
+  ],
   extends: [
     "airbnb-base",
     "airbnb-typescript/base",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:eslint-comments/recommended",
+    "plugin:jest/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
     "prettier",
   ],
   env: {
     node: true,
     browser: true,
+    jest: true,
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    sourceType: "module",
-    tsconfigRootDir: __dirname,
     project: "./tsconfig.json",
   },
-}
+  rules: {
+    "no-console": "off",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        // Allow CJS until ESM support improves
+        "@typescript-eslint/no-var-requires": "off",
+        "unicorn/prefer-module": "off",
+      },
+    },
+  ],
+};
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,36 @@
-import * as dotenv from "dotenv"
-import "module-alias/register"
-import Koa from "koa"
-import koaBody from "koa-body"
-import error from "koa-json-error"
-import Mongoose from "mongoose"
-import { networkInterfaces } from "os"
-import arcadesRoute from "routes/arcades"
-import mainRoute from "routes/index"
-import usersRoute from "routes/users"
+import * as dotenv from "dotenv";
+import "module-alias/register";
+import Koa from "koa";
+import koaBody from "koa-body";
+import error from "koa-json-error";
+import Mongoose from "mongoose";
+import { networkInterfaces } from "node:os";
+import arcadesRoute from "routes/arcades";
+import mainRoute from "routes/index";
+import usersRoute from "routes/users";
 
-dotenv.config({ path: ".env" })
+dotenv.config({ path: ".env" });
 
 // conect to DB
-const connectionString = process.env.MONGO_ATLAS_STRING
+const connectionString = process.env.MONGO_ATLAS_STRING;
 if (connectionString) {
-  Mongoose.connect(connectionString)
+  Mongoose.connect(connectionString);
 }
-Mongoose.connection.on("error", console.error)
+Mongoose.connection.on("error", console.error);
 
 // init koa server
-const app = new Koa()
-const port = process.env.PORT || 3000
+const app = new Koa();
+const port = process.env.PORT || 3000;
 
 // get the local network address
-let address: string
+let address: string;
 
 networkInterfaces().en0?.filter((details) => {
   if (details.family === "IPv4") {
-    address = details.address
+    address = details.address;
   }
-  return address
-})
+  return address;
+});
 
 app
   .use(
@@ -46,10 +46,10 @@ app
   .use(arcadesRoute.allowedMethods())
   .use(usersRoute.routes())
   .use(usersRoute.allowedMethods())
-  .use(error())
+  .use(error());
 
 // start server
 app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`)
-  console.log(`local network http://${address}:${port}`)
-})
+  console.log(`listening at http://localhost:${port}`);
+  console.log(`local network http://${address}:${port}`);
+});
